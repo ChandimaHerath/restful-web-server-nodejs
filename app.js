4d32f3c219a81a4e5dea0d291bee704d753a6efa
ChandimaHerath <chandimaherath@hotmail.com>
2021-11-10 21:29:12 +0530

    joi installed for validation

--- app.js
+++ app.js
@@ -29,12 +29,13 @@ app.get('/api/courses/:id', (req,res)=>{
 
 //Post Method
 app.post('/api/courses', (req,res)=>{
-    if (!req.body.name||req.body.name.length<3){
-        //400 bad request
-        res.status(400).send('Name is required and name should be larger than 3 characters');
-        return;
-    }
+  if (!req.body.name||req.body.name.length<3){
+        
+         res.status(400).send('Name is required and name should be larger than 3 characters');
+         return;
+     }
     
+
     const course = {  
         id: courses.length+1,
         name: req.body.name
